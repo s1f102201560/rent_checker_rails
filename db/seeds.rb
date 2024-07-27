9edc6f9138e04578e5f6d3b8eac59ce112d44be5
1F10220156 <s1f102201560@iniad.org>
2024-07-27 14:45:55 +0900

    :sparkles: add users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+users_lst = []
+
+10.times do |i|
+  name = "user#{i+1}"
+  email = "#{name}@example.com"
+  users_lst << { name: name, email: email }
+end
+
+User.create!(users_lst)
+
